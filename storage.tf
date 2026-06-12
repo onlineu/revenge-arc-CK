40cd985d0b1959b89d12efecfe89b5ff9ba60e60
Aleksei Dmitry <mnghiatran04@gmail.com>
2026-06-12 16:39:16 +0700

    Blocked public access to S3

--- storage.tf
+++ storage.tf
@@ -1,3 +1,17 @@
 resource "aws_s3_bucket" "test" {
     bucket = "local-test-bucket"
-}   
+
+    tags = {
+        Name = "Test Storage"
+        Environment = "LocalTesting"
+    }
+}
+
+resource "aws_s3_public_access_block" "storage_privacy" {
+    bucket = aws_s3_bucket.test.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
